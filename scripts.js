2c432a994ca3a6f4c3cc54536e94a45a42a22ffa
yiannir <yiannis.rav@gmail.com>
2024-06-04 13:55:55 +0100

    More improvements.

--- scripts.js
+++ scripts.js
@@ -35,7 +35,7 @@ const Utils = {
 };
 
 const WebsiteManager = {
-    grids: Utils.loadFromLocalStorage('grids'),
+    gridContainers: Utils.loadFromLocalStorage('gridContainers'),
 
     init: function () {
         this.cacheDom();
@@ -46,9 +46,9 @@ const WebsiteManager = {
     cacheDom: function () {
         this.editModeButton = document.getElementById('editModeButton');
         this.editMode = document.querySelector('.edit-mode');
-        this.gridsContainer = document.getElementById('gridsContainer');
+        this.gridContainersWrapper = document.getElementById('gridContainersWrapper');
         this.addWebsiteButton = document.getElementById('addWebsiteButton');
-        this.addGridButton = document.getElementById('addGridButton');
+        this.addGridContainerButton = document.getElementById('addGridContainerButton');
         this.websiteNameInput = document.getElementById('websiteName');
         this.websiteUrlInput = document.getElementById('websiteUrl');
         this.faviconUrlInput = document.getElementById('faviconUrl');
@@ -57,11 +57,11 @@ const WebsiteManager = {
     bindEvents: function () {
         this.editModeButton.addEventListener('click', this.toggleMode.bind(this));
         this.addWebsiteButton.addEventListener('click', this.addWebsite.bind(this));
-        this.addGridButton.addEventListener('click', this.addGrid.bind(this));
-        this.gridsContainer.addEventListener('click', this.handleGridClick.bind(this));
-        this.gridsContainer.addEventListener('dragstart', this.onDragStart.bind(this));
-        this.gridsContainer.addEventListener('dragover', this.onDragOver.bind(this));
-        this.gridsContainer.addEventListener('drop', this.onDrop.bind(this));
+        this.addGridContainerButton.addEventListener('click', this.addGridContainer.bind(this));
+        this.gridContainersWrapper.addEventListener('click', this.handleGridContainerClick.bind(this));
+        this.gridContainersWrapper.addEventListener('dragstart', this.onDragStart.bind(this));
+        this.gridContainersWrapper.addEventListener('dragover', this.onDragOver.bind(this));
+        this.gridContainersWrapper.addEventListener('drop', this.onDrop.bind(this));
     },
 
     render: function () {
@@ -86,34 +86,79 @@ const WebsiteManager = {
 
     toggleEditMode: function () {
         this.editMode.style.display = 'block';
-        this.renderGridsForEdit();
+        this.addGridContainerButton.style.display = 'block';
+        document.querySelectorAll('.addGridButton').forEach(button => {
+            button.style.display = 'flex';
+        });
+        this.makeDraggable(true);
+        this.renderGridContainersForEdit();
     },
 
     toggleRenderMode: function () {
         this.editMode.style.display = 'none';
-        this.renderGrids();
+        this.addGridContainerButton.style.display = 'none';
+        document.querySelectorAll('.addGridButton').forEach(button => {
+            button.style.display = 'none';
+        });
+        this.makeDraggable(false);
+        this.renderGridContainers();
     },
 
-    renderGrids: function () {
-        this.gridsContainer.innerHTML = '';
-        this.grids.forEach((grid, gridIndex) => {
-            const gridElement = this.createGridElement(grid, gridIndex, false);
-            this.gridsContainer.appendChild(gridElement);
+    makeDraggable: function (isEditable) {
+        const draggableItems = document.querySelectorAll('.grid-item');
+        draggableItems.forEach(item => {
+            item.setAttribute('draggable', isEditable);
         });
     },
 
-    renderGridsForEdit: function () {
-        this.gridsContainer.innerHTML = '';
-        this.grids.forEach((grid, gridIndex) => {
-            const gridElement = this.createGridElement(grid, gridIndex, true);
-            this.gridsContainer.appendChild(gridElement);
+    renderGridContainers: function () {
+        this.gridContainersWrapper.innerHTML = '';
+        this.gridContainers.forEach((container, containerIndex) => {
+            const containerElement = this.createGridContainerElement(container, containerIndex, false);
+            this.gridContainersWrapper.appendChild(containerElement);
         });
     },
 
-    createGridElement: function (grid, gridIndex, isEditMode) {
+    renderGridContainersForEdit: function () {
+        this.gridContainersWrapper.innerHTML = '';
+        this.gridContainers.forEach((container, containerIndex) => {
+            const containerElement = this.createGridContainerElement(container, containerIndex, true);
+            this.gridContainersWrapper.appendChild(containerElement);
+        });
+    },
+
+    createGridContainerElement: function (container, containerIndex, isEditMode) {
+        const containerElement = document.createElement('div');
+        containerElement.classList.add('grid-container');
+        containerElement.setAttribute('id', `gridContainer${containerIndex}`);
+
+        const gridsWrapper = document.createElement('div');
+        gridsWrapper.classList.add('grids-wrapper');
+        gridsWrapper.setAttribute('id', `gridsWrapper${containerIndex}`);
+
+        container.grids.forEach((grid, gridIndex) => {
+            const gridElement = this.createGridElement(grid, containerIndex, gridIndex, isEditMode);
+            gridsWrapper.appendChild(gridElement);
+        });
+
+        if (isEditMode && container.grids.length < 5) {
+            const addGridButton = document.createElement('button');
+            addGridButton.classList.add('addGridButton', 'add-grid-button');
+            addGridButton.setAttribute('data-container-id', containerIndex);
+            addGridButton.innerHTML = '<i class="fas fa-plus"></i>';
+            gridsWrapper.appendChild(addGridButton); // Ensure the button is added at the end
+        }
+
+        containerElement.appendChild(gridsWrapper);
+
+        return containerElement;
+    },
+
+    createGridElement: function (grid, containerIndex, gridIndex, isEditMode) {
         const gridElement = document.createElement('div');
         gridElement.classList.add('grid-column');
         gridElement.setAttribute('data-grid-index', gridIndex);
+        gridElement.setAttribute('data-container-index', containerIndex);
 
         const titleElement = document.createElement('div');
         titleElement.classList.add('grid-title');
@@ -121,7 +166,7 @@ const WebsiteManager = {
         if (isEditMode) {
             titleElement.setAttribute('contenteditable', 'true');
             titleElement.addEventListener('blur', () => {
-                this.updateGridTitle(gridIndex, titleElement.textContent);
+                this.updateGridTitle(containerIndex, gridIndex, titleElement.textContent);
             });
         }
 
@@ -130,9 +175,10 @@ const WebsiteManager = {
         const websitesContainer = document.createElement('div');
         websitesContainer.classList.add('grid');
         websitesContainer.setAttribute('data-grid-index', gridIndex);
+        websitesContainer.setAttribute('data-container-index', containerIndex);
 
         grid.websites.forEach((website, websiteIndex) => {
-            const websiteElement = this.createWebsiteElement(website, gridIndex, websiteIndex, isEditMode);
+            const websiteElement = this.createWebsiteElement(website, containerIndex, gridIndex, websiteIndex, isEditMode);
             websitesContainer.appendChild(websiteElement);
         });
 
@@ -141,21 +187,22 @@ const WebsiteManager = {
         return gridElement;
     },
 
-    createWebsiteElement: function (website, gridIndex, websiteIndex, isEditMode) {
+    createWebsiteElement: function (website, containerIndex, gridIndex, websiteIndex, isEditMode) {
         const websiteElement = document.createElement('div');
         websiteElement.classList.add('grid-item');
-        websiteElement.setAttribute('draggable', 'true');
+        websiteElement.setAttribute('draggable', isEditMode);
         websiteElement.setAttribute('data-grid-index', gridIndex);
+        websiteElement.setAttribute('data-container-index', containerIndex);
         websiteElement.setAttribute('data-website-index', websiteIndex);
 
         const favicon = website.favicon ? `<img src="${website.favicon}" alt="Favicon">` : '';
         if (isEditMode) {
             websiteElement.innerHTML = `
                 ${favicon}
-                <input type="text" value="${website.name}" data-grid-index="${gridIndex}" data-website-index="${websiteIndex}" class="website-name">
-                <input type="text" value="${website.url}" data-grid-index="${gridIndex}" data-website-index="${websiteIndex}" class="website-url">
-                <input type="text" value="${website.favicon || ''}" data-grid-index="${gridIndex}" data-website-index="${websiteIndex}" class="favicon-url" placeholder="Favicon URL">
-                <button class="delete-button" data-grid-index="${gridIndex}" data-website-index="${websiteIndex}">X</button>
+                <input type="text" value="${website.name}" data-grid-index="${gridIndex}" data-container-index="${containerIndex}" data-website-index="${websiteIndex}" class="website-name">
+                <input type="text" value="${website.url}" data-grid-index="${gridIndex}" data-container-index="${containerIndex}" data-website-index="${websiteIndex}" class="website-url">
+                <input type="text" value="${website.favicon || ''}" data-grid-index="${gridIndex}" data-container-index="${containerIndex}" data-website-index="${websiteIndex}" class="favicon-url" placeholder="Favicon URL">
+                <button class="delete-button" data-grid-index="${gridIndex}" data-container-index="${containerIndex}" data-website-index="${websiteIndex}">X</button>
             `;
         } else {
             websiteElement.innerHTML = `
@@ -180,44 +227,58 @@ const WebsiteManager = {
                 favicon = Utils.getFaviconUrl(url);
             }
             const newWebsite = { name, url, favicon };
-            const activeGrid = this.grids[0];
+            const activeContainer = this.gridContainers[0];
+            const activeGrid = activeContainer.grids[0];
             activeGrid.websites.push(newWebsite);
             this.websiteNameInput.value = '';
             this.websiteUrlInput.value = '';
             this.faviconUrlInput.value = '';
-            Utils.saveToLocalStorage('grids', this.grids);
-            this.renderGridsForEdit();
+            Utils.saveToLocalStorage('gridContainers', this.gridContainers);
+            this.renderGridContainersForEdit();
             Utils.showMessage('Website added successfully!', 'success');
         } else {
             Utils.showMessage('Please enter a valid website name and URL.', 'error');
         }
     },
 
-    addGrid: function () {
-        const newGrid = { title: 'New Grid', websites: [] };
-        this.grids.push(newGrid);
-        Utils.saveToLocalStorage('grids', this.grids);
-        this.renderGridsForEdit();
-        Utils.showMessage('New grid added successfully!', 'success');
+    addGridContainer: function () {
+        const newContainer = { grids: [] };
+        this.gridContainers.push(newContainer);
+        Utils.saveToLocalStorage('gridContainers', this.gridContainers);
+        this.renderGridContainersForEdit();
+        Utils.showMessage('New grid container added successfully!', 'success');
     },
 
-    updateGridTitle: function (gridIndex, newTitle) {
-        this.grids[gridIndex].title = newTitle;
-        Utils.saveToLocalStorage('grids', this.grids);
+    addGrid: function (containerIndex) {
+        if (this.gridContainers[containerIndex].grids.length < 5) {
+            const newGrid = { title: 'New Grid', websites: [] };
+            this.gridContainers[containerIndex].grids.push(newGrid);
+            Utils.saveToLocalStorage('gridContainers', this.gridContainers);
+            this.renderGridContainersForEdit();
+            Utils.showMessage('New grid added successfully!', 'success');
+        } else {
+            Utils.showMessage('Maximum of 5 grids per container reached.', 'error');
+        }
     },
 
-    deleteWebsite: function (gridIndex, websiteIndex) {
-        this.grids[gridIndex].websites.splice(websiteIndex, 1);
-        Utils.saveToLocalStorage('grids', this.grids);
-        this.renderGridsForEdit();
+    updateGridTitle: function (containerIndex, gridIndex, newTitle) {
+        this.gridContainers[containerIndex].grids[gridIndex].title = newTitle;
+        Utils.saveToLocalStorage('gridContainers', this.gridContainers);
+    },
+
+    deleteWebsite: function (containerIndex, gridIndex, websiteIndex) {
+        this.gridContainers[containerIndex].grids[gridIndex].websites.splice(websiteIndex, 1);
+        Utils.saveToLocalStorage('gridContainers', this.gridContainers);
+        this.renderGridContainersForEdit();
         Utils.showMessage('Website deleted successfully!', 'success');
     },
 
     onDragStart: function (e) {
         this.draggedItemIndex = e.target.getAttribute('data-website-index');
         this.draggedGridIndex = e.target.getAttribute('data-grid-index');
+        this.draggedContainerIndex = e.target.getAttribute('data-container-index');
         e.dataTransfer.effectAllowed = 'move';
-        e.dataTransfer.setData('text/plain', `${this.draggedGridIndex},${this.draggedItemIndex}`);
+        e.dataTransfer.setData('text/plain', `${this.draggedContainerIndex},${this.draggedGridIndex},${this.draggedItemIndex}`);
         e.target.classList.add('draggable');
     },
 
@@ -228,10 +289,11 @@ const WebsiteManager = {
 
     onDrop: function (e) {
         e.preventDefault();
-        const [fromGridIndex, fromWebsiteIndex] = e.dataTransfer.getData('text/plain').split(',').map(Number);
+        const [fromContainerIndex, fromGridIndex, fromWebsiteIndex] = e.dataTransfer.getData('text/plain').split(',').map(Number);
         const targetGridElement = e.target.closest('.grid-column');
 
         if (targetGridElement) {
+            const toContainerIndex = Number(targetGridElement.getAttribute('data-container-index'));
             const toGridIndex = Number(targetGridElement.getAttribute('data-grid-index'));
             const targetWebsiteElement = e.target.closest('.grid-item');
 
@@ -240,33 +302,37 @@ const WebsiteManager = {
                 toWebsiteIndex = Number(targetWebsiteElement.getAttribute('data-website-index'));
             }
 
-            this.moveWebsite(fromGridIndex, fromWebsiteIndex, toGridIndex, toWebsiteIndex);
+            this.moveWebsite(fromContainerIndex, fromGridIndex, fromWebsiteIndex, toContainerIndex, toGridIndex, toWebsiteIndex);
         } else {
             Utils.showMessage('Drop target is invalid.', 'error');
         }
 
-        this.gridsContainer.querySelectorAll('.draggable').forEach(el => el.classList.remove('draggable'));
+        this.gridContainersWrapper.querySelectorAll('.draggable').forEach(el => el.classList.remove('draggable'));
     },
 
-    moveWebsite: function (fromGridIndex, fromWebsiteIndex, toGridIndex, toWebsiteIndex) {
-        const draggedItem = this.grids[fromGridIndex].websites.splice(fromWebsiteIndex, 1)[0];
+    moveWebsite: function (fromContainerIndex, fromGridIndex, fromWebsiteIndex, toContainerIndex, toGridIndex, toWebsiteIndex) {
+        const draggedItem = this.gridContainers[fromContainerIndex].grids[fromGridIndex].websites.splice(fromWebsiteIndex, 1)[0];
 
         if (toWebsiteIndex !== null && toWebsiteIndex !== undefined) {
-            this.grids[toGridIndex].websites.splice(toWebsiteIndex, 0, draggedItem);
+            this.gridContainers[toContainerIndex].grids[toGridIndex].websites.splice(toWebsiteIndex, 0, draggedItem);
         } else {
-            this.grids[toGridIndex].websites.push(draggedItem);
+            this.gridContainers[toContainerIndex].grids[toGridIndex].websites.push(draggedItem);
         }
 
-        Utils.saveToLocalStorage('grids', this.grids);
-        this.renderGridsForEdit();
+        Utils.saveToLocalStorage('gridContainers', this.gridContainers);
+        this.renderGridContainersForEdit();
         Utils.showMessage('Website moved successfully!', 'success');
     },
 
-    handleGridClick: function (e) {
-        if (e.target.classList.contains('delete-button')) {
+    handleGridContainerClick: function (e) {
+        if (e.target.closest('.addGridButton')) {
+            const containerIndex = e.target.closest('.addGridButton').getAttribute('data-container-id');
+            this.addGrid(containerIndex);
+        } else if (e.target.classList.contains('delete-button')) {
+            const containerIndex = e.target.getAttribute('data-container-index');
             const gridIndex = e.target.getAttribute('data-grid-index');
             const websiteIndex = e.target.getAttribute('data-website-index');
-            this.deleteWebsite(gridIndex, websiteIndex);
+            this.deleteWebsite(containerIndex, gridIndex, websiteIndex);
         }
     }
 };
